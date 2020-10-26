54febc48bbdf28d4f141c36408efcac9f787bf6f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-26 17:39:13 +0100

    wip

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log('AWESOME Web 35!!!!!!')
-// dependencies 
+// 1- DEPENDENCIES
 // import express from 'express' // package.json needs a "type" key with a value of "module"
 const express = require('express') // commonjs module system that came with Node
+// import { generate } from 'shortid' // ES6 modules
 const generate = require('shortid').generate
+
+// 2- INSTANTIATE AND CONFIGURE THE SERVER
+
