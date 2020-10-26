bad78af26b70bbae7fa23b499745e420e6ebaec1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-26 18:13:05 +0100

    wip

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/dogs', (req, res) => {
 
 app.get('/dogs/:id', (req, res) => {
   // 1- pull out the id from the request (the URL param)
+  const { id } = req.params
 })
 
 //  [GET] catch all endpoint (404 resource not found)
